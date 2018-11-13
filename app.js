7df443ef73ed3a32486d2c766f6ba31e67c75f0c
akhilarao2024 <akhilarao2024@gmail.com>
2018-11-13 12:47:18 -0600

    module1 assignment

---
+++ app.js
@@ -0,0 +1,33 @@
+(function() {
+    'use strict';
+
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+        $scope.dishes = '';
+        $scope.message = '';
+        $scope.checked = false;
+
+        $scope.checkLunch = function() {
+            if ($scope.dishes.trim().length === 0) {
+                $scope.empty = true;
+            } else {
+                $scope.checked = true;
+                $scope.empty = false;
+                var dishes1 = $scope.dishes.split(',');
+                var dishesWithoutEmpty = dishes1.filter(function(dish) {
+                    return dish.trim() !== '';
+                });
+
+                if (dishesWithoutEmpty.length <= 3) {
+                    $scope.message = 'Enjoy!';
+                } else {
+                    $scope.message = 'Too much!';
+                }
+            }
+        };
+    }
+})();
